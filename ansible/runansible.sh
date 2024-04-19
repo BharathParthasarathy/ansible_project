3bb4e8c0ac75cbba587ab583181d05bf2323d7bf
BharathParthasarathy <146115839+BharathParthasarathy@users.noreply.github.com>
2024-04-19 15:24:00 -0400

    Add files via upload

---
+++ ansible/runansible.sh
@@ -0,0 +1,10 @@
+cd /home/bharath/automation/ansible/ansible_project/ansible  # Change the path to the location of the ansible playbook
+echo "------------------------------------"
+echo "Playbook Execution starting"
+echo "------------------------------------"
+
+ansible-playbook --verbose n01609484-playbook.yaml
+
+echo "------------------------------------"
+echo "Playbook Execution Completed"
+echo "------------------------------------"
